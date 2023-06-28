96915f6985c368aed9e6453292e3ea68037f759d
arhscrypt <93065649+arhscrypt@users.noreply.github.com>
2023-06-28 21:21:28 +0700

    Add search input and button

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,42 +3,43 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div className="navbar bg-base-100">
-        <div className="flex-1">
-          <a className="btn btn-ghost normal-case text-xl">daisyUI</a>
-        </div>
-        <div className="flex-none gap-2">
-          <div className="form-control">
+      <div className="flex min-h-screen flex-col items-center justify-between">
+        <div className="form-control w-screen p-8">
+          <div className="input-group">
             <input
               type="text"
-              placeholder="Search"
-              className="input input-bordered w-24 md:w-auto"
+              placeholder="Enter username.."
+              className="input input-bordered w-screen"
             />
+            <button className="btn btn-square">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-6 w-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+            </button>
           </div>
-          <div className="dropdown dropdown-end">
-            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
-              <div className="w-10 rounded-full">
-                <img src="https://picsum.photos/200" />
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
-            >
-              <li>
-                <a className="justify-between">
-                  Profile
-                  <span className="badge">New</span>
-                </a>
-              </li>
-              <li>
-                <a>Settings</a>
-              </li>
-              <li>
-                <a>Logout</a>
-              </li>
-            </ul>
-          </div>
+          <ul className="menu bg-base-300 rounded-box mt-4">
+            <li>
+              <details>
+                <summary>username</summary>
+                <ul>
+                  <li className="bg-base-200">
+                    <a>repository</a>
+                  </li>
+                </ul>
+              </details>
+            </li>
+          </ul>
         </div>
       </div>
     </>
