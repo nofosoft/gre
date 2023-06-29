68664dfcc8f586fa7a56ea3fe408f4a6227f7c3d
arhscrypt <93065649+arhscrypt@users.noreply.github.com>
2023-06-29 16:21:19 +0700

    Add element functions

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,9 +1,10 @@
 import "./App.css";
 import Heading from "./components/Heading";
 import Content from "./components/Content";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { Octokit } from "@octokit/rest";
 
 function App() {
   const [typingKeys, setTypingKeys] = useState<string>("");
@@ -60,9 +61,97 @@ function App() {
     setTypingKeys(event.target.value);
   };
 
+  const onHandleSearch = async () => {
+    // console.log("onHandleSearch");
+    getUsernameData(typingKeys);
+  };
+
+  interface userObj {
+    id: number;
+    name: any;
+  }
+  const [username, setUsername] = useState<userObj[]>([]);
+
+  const getUsernameData = async (keywords: string) => {
+    setUsername([]);
+    const octokit = new Octokit({
+      auth: "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",
+    });
+    const {
+      data: {},
+    } = await octokit.rest.users.getAuthenticated();
+    // console.log("Hello, %s", login);
+
+    try {
+      const get_user = await octokit.search.users({
+        q: keywords,
+      });
+
+      const { items } = get_user.data;
+      items.map((item, idx) => {
+        if (idx < 5) {
+          const newObject: userObj = {
+            id: idx,
+            name: item.login,
+          };
+          setUsername((prevState) => [...prevState, newObject]);
+        }
+      });
+    } catch (error) {
+      // console.error('Error:', error);
+    }
+  };
+
+  interface repoObj {
+    id: number;
+    repos: any;
+  }
+  const [repos, setRepos] = useState<repoObj[]>([]);
+
+  const getReposData = async (keywords: string) => {
+    setRepos([]);
+    const octokit = new Octokit({
+      auth: "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",
+    });
+    const {
+      data: {},
+    } = await octokit.rest.users.getAuthenticated();
+    // console.log("Hello, %s", login);
+
+    try {
+      const response = await octokit.request("GET /users/{user}/repos", {
+        user: keywords,
+      });
+
+      const details = response.data;
+      details.map((items: any, idx: number) => {
+        const newObject: repoObj = {
+          id: idx,
+          repos: items.name,
+        };
+        setRepos((prevState) => [...prevState, newObject]);
+      });
+    } catch (error) {
+      //console.error('Error:', error);
+    }
+  };
+
+  useEffect(() => {
+    username.map((item, idx) => {
+      console.log(item);
+      getReposData(item.name);
+    });
+    repos.map((item, idx) => {
+      console.log(item);
+    });
+  }, [username, repos]);
+
   return (
     <>
-      <Heading onHandleTypeing={onHandleTypeing} />
+      <Heading
+        onHandleTypeing={onHandleTypeing}
+        onHandleSearch={onHandleSearch}
+      />
       <Content search={typingKeys} founded={0} dataUsers={dataUsers} />
     </>
   );
--- app/src/components/Heading.tsx
+++ app/src/components/Heading.tsx
@@ -2,9 +2,13 @@ import { ReactElement, useEffect, useState } from "react";
 
 type HeadingProps = {
   onHandleTypeing: any;
+  onHandleSearch: any;
 };
 
-const Heading = ({ onHandleTypeing }: HeadingProps): ReactElement => {
+const Heading = ({
+  onHandleTypeing,
+  onHandleSearch,
+}: HeadingProps): ReactElement => {
   const [theme, setTheme] = useState("light");
 
   const toggleTheme = () => {
@@ -24,21 +28,8 @@ const Heading = ({ onHandleTypeing }: HeadingProps): ReactElement => {
         className="input input-ghost w-screen input-bordered mr-2"
         onChange={onHandleTypeing}
       />
-      <button className="btn btn-ghost btn-circle">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          className="h-5 w-5"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke="currentColor"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-          />
-        </svg>
+      <button onClick={onHandleSearch} className="btn btn-ghost btn-circle">
+        <i className="fa-solid fa-magnifying-glass fa-xl"></i>
       </button>
       <div className="mx-2">
         <i className="fa-solid fa-circle-half-stroke"></i>
